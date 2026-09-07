183530513406a887ee4dc5c0f0b0622dea72bdd4
SatyamBhosale2004 <satyambhosale.work@gmail.com>
2026-09-07 23:50:23 +0530

    Feat: day 11 diamond prblm

---
+++ c++/day11/diamPrblm.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class A{
+public:
+	int a;
+};
+
+class B : virtual public A{
+public:
+	int b;
+
+};
+
+class C : virtual public A{
+public:
+	int c;
+
+};
+
+// mutliple inheritance
+class D : public C, public B{
+public :
+	int d;
+};
+
+int main() {
+
+	D dobj;
+
+//	dobj.a;	// without virtual error: request for member 'a' is ambiguous
+
+	dobj.a = 10;
+	cout << dobj.a << endl;
+
+	return 0;
+}
